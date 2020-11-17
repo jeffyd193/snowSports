72552a7dd5d859ed3afc4bec6bbdada86dce0a0e
JeffyD <62564419+jeffyd193@users.noreply.github.com>
2020-11-17 16:30:36 -0600

    added sales and inventory category img's

--- js/app.js
+++ js/app.js
@@ -8,3 +8,39 @@ $(document).ready(function(){
   
 	});
 });
+
+(function() {
+  const pictures = [
+    "ski1",
+    "ski2",
+    "board1",
+    "board2",
+    "snowmobile1"
+  ];
+
+  //select both left and right button. Add event listeners
+  const buttons = document.querySelectorAll('.btn')
+  console.log(buttons)
+  const imageDIV = document.querySelector('.content')
+  console.log(imageDIV)
+  let counter = 0
+
+  buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+      if (button.classList.contains('leftBtn')){
+        counter--
+        if(counter < 0){
+          counter = pictures.length -1
+        }
+        imageDIV.style.backgroundImage = `url('../img/${pictures[counter]}.jpg')`
+      }
+      if (button.classList.contains('rightBtn')){
+        counter++
+        if(counter > pictures.length -1){
+          counter = 0
+        }
+        imageDIV.style.backgroundImage = `url('../img/${pictures[counter]}.jpg')`
+      }
+    })
+  })
+})();
