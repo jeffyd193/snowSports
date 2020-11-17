3918506de848e553a48066d287c321e042a059c5
JeffyD <62564419+jeffyd193@users.noreply.github.com>
2020-11-16 20:28:26 -0600

    initialTestPage

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+        $(this).toggleClass('open');
+        
+    
+    $('.container').toggleClass('hide');
+    $('.container').toggleClass('show');
+  
+	});
+});
